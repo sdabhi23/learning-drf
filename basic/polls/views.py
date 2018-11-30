4a6cb80e5ff9ce09a4a93dc113d4bfc7336d2328
Shrey <shrey.dabhi23@gmail.com>
2018-11-30 14:38:45 +0530

    add homepage

--- basic/polls/views.py
+++ basic/polls/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
